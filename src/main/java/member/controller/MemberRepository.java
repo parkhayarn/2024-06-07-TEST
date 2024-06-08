efa4582eb094d47f963e2bc3ebbebe2d5658f4e2
parkhayarn <s0224y92@gmail.com>
2024-06-08 11:41:46 +0900

    MemberRepository

--- src/main/java/member/controller/MemberRepository.java
+++ src/main/java/member/controller/MemberRepository.java
@@ -1,12 +1,27 @@
 package member.controller;
 
+import member.model.vo.Gold;
 import member.model.vo.Member;
 import member.model.vo.Silver;
+import member.model.vo.Vip;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberRepository {
 
-    public void insertMember(Member member) {
+    private static final int MAX_MEMBERS = 10;
+    private List<Member> silverMembers;
+    private List<Member> goldMembers;
+    private List<Member> vipMembers;
+
+    public MemberRepository() {
+        silverMembers = new ArrayList<>();
+        goldMembers = new ArrayList<>();
+        vipMembers = new ArrayList<>();
     }
 
-
+    public void insertMember(Member m) {
+        if (m >= 10)
+    }
 }
