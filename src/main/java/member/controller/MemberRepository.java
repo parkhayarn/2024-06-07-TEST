ae69d1c8290fea4a93ad97530af1f4d02e0172e1
parkhayarn <s0224y92@gmail.com>
2024-06-08 16:54:23 +0900

    포인트이율 계산 결과 출력 화면

--- src/main/java/member/controller/MemberRepository.java
+++ src/main/java/member/controller/MemberRepository.java
@@ -4,24 +4,37 @@ import member.model.vo.Gold;
 import member.model.vo.Member;
 import member.model.vo.Silver;
 import member.model.vo.Vip;
+import member.run.Run;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MemberRepository {
 
     private static final int MAX_MEMBERS = 10;
-    private List<Member> silverMembers;
-    private List<Member> goldMembers;
-    private List<Member> vipMembers;
-
-    public MemberRepository() {
-        silverMembers = new ArrayList<>();
-        goldMembers = new ArrayList<>();
-        vipMembers = new ArrayList<>();
-    }
+    List<Member> members = new ArrayList<>();
 
     public void insertMember(Member m) {
-        if (m >= 10)
+        members.add(m);
+        try{
+            if (members.size() >= MAX_MEMBERS){
+                throw new OverMemberException();
+            };
+        } catch (Exception e){}
+
+        }
+
+    public void printData() {
+//        System.out.println(members.size());
+        for (Member m : members) {
+            if(m.getGrade() == "Silver") {
+                System.out.println(m.getGrade() + "등급 회원 " + m.getName() + "의 포인트는" + m.getPoint() + "이고, 이자포인트는" + m.calculateInterest() + "입니다.");
+            }else if(m.getGrade() == "Gold"){
+                System.out.println(m.getGrade() + "등급 회원 " + m.getName() + "의 포인트는" + m.getPoint() + "이고, 이자포인트는" + m.calculateInterest() + "입니다.");
+            } else if(m.getGrade() == "Vip") {
+                System.out.println(m.getGrade() + "등급 회원 " + m.getName() + "의 포인트는" + m.getPoint() + "이고, 이자포인트는" + m.calculateInterest() + "입니다.");
+            }
+            }
+        }
     }
-}
