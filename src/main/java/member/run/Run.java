17f73ed2112dbaefb08d1e0bb9f3cf9e8bc1660e
parkhayarn <s0224y92@gmail.com>
2024-06-11 21:55:45 +0900

    문구 수정ㅇ

--- src/main/java/member/run/Run.java
+++ src/main/java/member/run/Run.java
@@ -27,7 +27,7 @@ public class Run {
             System.out.println("=========================== 회원 포인트 적립 정보 ===========================");
             memberRepository.printData();
 
-            System.out.println("=========================== 회원 구매 정보 ===========================");
+            System.out.println("=========================== 회원 물품 금액 정보 ===========================");
             int price = 10000;
             memberRepository.printBuyInfo(price);
 
